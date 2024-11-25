dea23d9f686d36cde69fba3f95f7311f8639b7f1
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-25 11:46:17 +0200

    feat: GlobalExceptionHandler

--- server/src/main/java/com/dreammy/server/middleware/GlobalExceptionHandler.java
+++ server/src/main/java/com/dreammy/server/middleware/GlobalExceptionHandler.java
@@ -1,20 +1,48 @@
 package com.dreammy.server.middleware;
 
+import com.dreammy.server.exceptions.MetricTypeNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MetricTypeNotFoundException.class)
+    public ResponseEntity<Object> handleMetricTypeNotFoundException(MetricTypeNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("error", "MetricType.NotFound");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        response.put("error", "InvalidArgument");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Invalid parameter type: " + ex.getName());
+        response.put("details", "Expected argument type: " + ex.getRequiredType());
+        response.put("error", "InvalidArgumentType");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handleUnexpectedException(Exception ex) {
         Map<String, String> response = new HashMap<>();
         response.put("message", "An unexpected error occurred!" + ex.getMessage());
         response.put("details", ex.toString());
+        response.put("error", "UnexpectedError");
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
