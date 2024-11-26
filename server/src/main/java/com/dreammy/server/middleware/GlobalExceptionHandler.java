b91987935d3f20a0bf47d21231f2372da116607e
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-26 18:17:25 +0200

    chore: separating exception handlers

--- server/src/main/java/com/dreammy/server/middleware/GlobalExceptionHandler.java
+++ server/src/main/java/com/dreammy/server/middleware/GlobalExceptionHandler.java
@@ -1,9 +1,10 @@
 package com.dreammy.server.middleware;
 
-import com.dreammy.server.exceptions.metricType.MetricTypeNotFoundException;
-import com.dreammy.server.exceptions.metricType.SeedingDefaultMetricTypesException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -11,48 +12,36 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import java.util.HashMap;
 import java.util.Map;
 
+@Order(Ordered.LOWEST_PRECEDENCE)
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(MetricTypeNotFoundException.class)
-    public ResponseEntity<Object> handleMetricTypeNotFoundException(MetricTypeNotFoundException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", ex.getMessage());
-        response.put("code", "MetricType.NotFound");
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", ex.getMessage());
-        response.put("code", "InvalidArgument");
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return buildResponse(ex.getMessage(), ex.toString(), "InvalidArgument", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Invalid parameter type: " + ex.getName());
-        response.put("details", "Expected argument type: " + ex.getRequiredType());
-        response.put("code", "InvalidArgumentType");
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        String details = "Expected argument type: " + ex.getRequiredType();
+        return buildResponse("Invalid parameter type: " + ex.getName(), details, "InvalidArgumentType", HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(SeedingDefaultMetricTypesException.class)
-    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(SeedingDefaultMetricTypesException ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Invalid condition");
-        response.put("details", "Expected condition: " + ex.getMessage());
-        response.put("code", "InvalidCondition");
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return buildResponse("Message is not readable", ex.toString(), "NotReadable", HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({Exception.class})
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleUnexpectedException(Exception ex) {
+        return buildResponse("An unexpected error occurred!", ex.toString(), "UnexpectedError", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<Object> buildResponse(String message, String details, String code, HttpStatus status) {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "An unexpected error occurred!" + ex.getMessage());
-        response.put("details", ex.toString());
-        response.put("code", "UnexpectedError");
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        response.put("message", message);
+        response.put("details", details);
+        response.put("code", code);
+        return new ResponseEntity<>(response, status);
     }
 }
