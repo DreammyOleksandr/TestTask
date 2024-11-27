e049a14ab3badad38036a367dc785d2d24ddb5ab
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-27 12:56:57 +0200

    feat: default layout

---
+++ client/src/app/components/layout/Header.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <Link className='navbar-brand' href='/'>
+        Metrics Fetcher
+      </Link>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarSupportedContent'
+        aria-controls='navbarSupportedContent'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+
+      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <ul className='navbar-nav mr-auto'>
+          <li className='nav-item active'>
+            <Link className='nav-link' href='/'>
+              Home <span className='sr-only'></span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
