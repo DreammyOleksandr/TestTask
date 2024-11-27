3458ee3fba43006cdb1bb3f77bc2b22092b088da
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-27 13:29:34 +0200

    feat: main page validation

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,9 +11,16 @@ import {
   deleteMetricTypes,
 } from './utils/api'
 
+interface ErrorResponse {
+  code: string
+  details: string
+  message: string
+}
+
 export default function Home() {
   const [metricValues, setMetricValues] = useState([])
   const [metricTypes, setMetricTypes] = useState([])
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   const fetchData = async () => {
     try {
@@ -30,8 +37,15 @@ export default function Home() {
       await createDefaultMetricTypes()
       console.log('Default types created')
       fetchData()
-    } catch (error) {
+      setErrorMessage(null)
+    } catch (error: unknown) {
       console.error('Error creating default types:', error)
+
+      if (isErrorResponse(error)) {
+        setErrorMessage(error.details)
+      } else {
+        setErrorMessage('To create default types you must delete all types')
+      }
     }
   }
 
@@ -64,6 +78,8 @@ export default function Home() {
 
   return (
     <div className='container mt-5'>
+      {errorMessage && <div className='alert alert-warning'>{errorMessage}</div>}
+
       <div className='row mb-5'>
         <div className='row'>
           <div className='col-md-4'>
@@ -94,5 +110,15 @@ export default function Home() {
       </div>
       <Table metricValues={metricValues} onDelete={handleDeleteValue} />
     </div>
+  )
+}
+
+// Type guard to check if the error is of type ErrorResponse
+function isErrorResponse(error: unknown): error is ErrorResponse {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'details' in error &&
+    typeof (error as ErrorResponse).details === 'string'
   )
 }
